613c3bbaea9029537ae45dc8e8f1cb1caa045f5c
Emmi Korhonen <142783862+emmi3s@users.noreply.github.com>
2024-01-11 19:02:10 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,38 +1,93 @@
 'use strict';
 
-
-
 //Filter-valikko
 
-// poistaa valikosta dress, skirt jos valitsee gender -male
-const selectGender = document.getElementById('gender');
+let patternSelected = '';
+let genderSelected = '';
+let groupSelected = '';
 
-selectGender.addEventListener('change', function () {
-    const selectedGender = selectGender.value;
+
+
+
+//filter-valikko
+const patternTypeSelect = document.getElementById('pattern-type');
+const genderSelect = document.getElementById('gender');
+const groupSelect = document.getElementById('group');
+
+
+
+patternTypeSelect.addEventListener('change', (event) => {
+    patternSelected = event.target.value;
+  /*  fetchImages(patternSelected, genderSelected, groupSelected) */
+});
+
+
+genderSelect.addEventListener('change', (event) => {
+    genderSelected = event.target.value;
+
+    // rajaa pois mekot ja hameet
     const dressesOption = document.querySelector('option[value="dress"]');
     const skirtsOption = document.querySelector('option[value="skirt"]');
 
-    if (selectedGender == 'male'){
+    if (genderSelected == 'male'){
         dressesOption.style.display = 'none';
         skirtsOption.style.display = 'none';
     } else {
         dressesOption.style.display = 'block';
         skirtsOption.style.display = 'block';
-    }}
-    )
+    }
+
+  /*  fetchImages(patternSelected, genderSelected, groupSelected) */
+});
+
+
+
+groupSelect.addEventListener('change', (event) => {
+    groupSelected = event.target.value;
+   /* fetchImages(patternSelected, genderSelected, groupSelected) */
+});
+
 
 
 
 
 
 async function fetchImages(start, batch) {
+
     let imagePaths = [];
+
+    fetch('patternsInfo.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(jsonData => {
+      // Extract image paths
+      imagePaths = jsonData.map(entry => entry.image_path);
+      console.log('List of image paths:', imagePaths);
+      //return imagePaths;
+      imagesToPage(imagePaths);
+      // Print the list of image paths
+
+    })
+    .catch(error => {
+      console.error('Error fetching or parsing JSON:', error);
+    });
+
+
+    console.log(imagePaths)
+
+
+    /*
+      let imagePaths = [];
 
     for (let i = start + 1; i <= start + batch; i++) {
         const imagePath = `items_directory/item${i}.jpg`;
         try {
             const response = await fetch(imagePath, { method: 'HEAD' });
-            //console.log(response.status)
+
             if (response.ok) {
                 imagePaths.push(imagePath);
             } else {
@@ -44,6 +99,7 @@ async function fetchImages(start, batch) {
     }
     console.log(imagePaths)
     return imagePaths;
+    */
 }
 
 
@@ -108,7 +164,7 @@ async function nextPage(pageNumber) {
         document.querySelector('.gallery').innerHTML = '';
     }
 
-    imagesToPage(images);
+
 }
 
 nextPage(currentPage);
